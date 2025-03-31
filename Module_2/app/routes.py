e0847a544b79b533129e3734e3f951ee16ba36b7
SINEdowskY <kub200105@gmail.com>
2025-03-31 23:28:40 +0200

    better path namings

--- Module_2/app/routes.py
+++ Module_2/app/routes.py
@@ -6,7 +6,7 @@ from app.utils import validate_file_size, validate_file_type
 
 router = APIRouter()
 
-@router.post("/text")
+@router.post("/file")
 async def extract_text_from_file(file: UploadFile):
     extension = file.filename.split(".")[-1].lower()
     content_type = file.headers.get('Content-Type')
@@ -27,3 +27,7 @@ async def extract_text_from_file(file: UploadFile):
         text=extracted_text,
         metadata=FileMetadata(filename=file.filename, size=file.size)
     )
+
+@router.post("/website")
+async def extract_text_from_website(website_url: str):
+    return {"message": "still empty :'("}
